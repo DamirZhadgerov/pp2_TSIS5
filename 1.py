512bddc422c22af8cf7d851def79a7b0950cd1ce
Damir Zhadgerov <d_zhadgerov@kbtu.kz>
2024-03-10 18:59:23 +0600

    commit

--- 1.py
+++ 1.py
@@ -1,17 +1,12 @@
 import re
 
-# Provide the full path to the file
 file_path = r'C:\Users\Damir\OneDrive\Рабочий стол\row.txt'
 
-# Open and read the file with utf-8 encoding
 with open(file_path, 'r', encoding='utf-8') as file:
     data = file.read()
 
-# Define the pattern
 pattern = 'ab*'
 
-# Find matches
 matches = re.findall(pattern, data)
-# Print matches
 for match in matches:
     print(match)
--- 3.py
+++ 3.py
@@ -1,11 +1,11 @@
 import re
 
-def match_two_to_three_b():
+def match_lowercase_letters():
     file_path = r'C:\Users\Damir\OneDrive\Рабочий стол\row.txt'
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read()
-    pattern = re.compile(r'ab{2,3}')
+    pattern = re.compile(r'[a-z]+_[a-z]+')
     matches = pattern.findall(data)
     return matches
 
-print("Task 2:", match_two_to_three_b())
+print("Task:", match_lowercase_letters())
